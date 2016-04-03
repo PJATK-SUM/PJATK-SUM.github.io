1601f08d952dbdc707ab522a2085988debf5e636
Patryk Ptasiński <patryk.ptasinski@ipepe.pl>
2016-04-03 13:35:14 +0200

    Poprawienie formatowania

--- js/landing-page.js
+++ js/landing-page.js
@@ -4,17 +4,17 @@ $('body').scrollspy({
 });
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+$('.navbar-collapse ul li a').click(function () {
     $('.navbar-toggle:visible').click();
 });
 
-$('div.modal').on('show.bs.modal', function() {
-	var modal = this;
-	var hash = modal.id;
-	window.location.hash = hash;
-	window.onhashchange = function() {
-		if (!location.hash){
-			$(modal).modal('hide');
-		}
-	}
+$('div.modal').on('show.bs.modal', function () {
+    var modal = this;
+    var hash = modal.id;
+    window.location.hash = hash;
+    window.onhashchange = function () {
+        if (!location.hash) {
+            $(modal).modal('hide');
+        }
+    }
 });
